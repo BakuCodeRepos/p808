f8a69422412120673bc285ee43ca87b6ba05c008
Isa Arifoglu <arifogluisa@gmail.com>
2023-11-24 21:12:34 +0400

    product models added

--- app/product/admin.py
+++ app/product/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Brand, Category, Color, Product, ProductItem, ProductType, Size
+
+
+class InlineSizeAdmin(admin.TabularInline):
+    model = Size
+    extra = 1
+
+
+class InlineColorAdmin(admin.TabularInline):
+    model = Color
+    extra = 1
+
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    inlines = (InlineSizeAdmin, InlineColorAdmin)
+
 
 admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductItem)
+admin.site.register(Brand)
